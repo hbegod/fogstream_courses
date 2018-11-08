919f002edf6e0b6b4388a710a7b8a25e09f0fc3a
KolesnikovRA <kolesnikovra@miac.medkhv.ru>
2018-11-08 16:15:29 +1000

    Practice 4 add

---
+++ Practice_4/Point3D.py
@@ -0,0 +1,14 @@
+# напишите реализацию класса Point3D
+import math
+
+
+class Point3D:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def distance(self, other):
+        if not isinstance(other, Point3D):
+            raise ValueError('В метод передан параметр неверного типа. Тип должен быть Point3D')
+        return math.sqrt(math.pow(other.x - self.x, 2) + math.pow(other.y - self.y, 2) + math.pow(other.z - self.z, 2))
---
+++ Practice_4/task_1.py
@@ -0,0 +1,28 @@
+from OOP.Fraction import Fraction
+from OOP.Figure import Figure
+from OOP.Figure import Circle
+from OOP.Figure import Rectangle
+from OOP.Figure import Triangle
+import math
+
+triangle = Triangle(1)
+
+# fraction1 = Fraction(0, 7)
+# fraction2 = Fraction(0, 1)
+#
+# bool_fra = fraction1 == fraction2
+# print(bool_fra)
+
+# a = fraction1 + fraction2
+# b = fraction1 - fraction2
+# c = fraction1 * fraction2
+# print()
+# a.print_info()
+# print(b)
+# c.print_info()
+
+
+
+
+
+
