edad14ab425d85e30e3431a6d35919b6765dbb03
KolesnikovRA <kolesnikovra@miac.medkhv.ru>
2018-11-23 20:42:26 +1000

    работа над 6 практикой

--- Practice_6/task_5.py
+++ Practice_6/task_5.py
@@ -1,10 +1,21 @@
-time_shift = 12 # time_shift хранит число дней сдвига
 from datetime import datetime
 from datetime import timedelta
-now_date = datetime.now().date()
-day_delta = timedelta(days=time_shift)
-result_date = now_date + day_delta
-print(now_date)
-print(result_date)
-result = result_date.isocalendar()[1]
+
+time_shift = 12  # time_shift хранит число дней сдвига
+
+
+def shift_days(days):
+    """
+    Function adds days to now_date and return result as number of week in which result date was
+    :param days:
+    :return: number of week
+    """
+    now_date = datetime.now().date()
+    day_delta = timedelta(days=days)
+    result_date = now_date + day_delta
+    return result_date.isocalendar()[1]
+
+
+result = shift_days(time_shift)
 print(result)
+
